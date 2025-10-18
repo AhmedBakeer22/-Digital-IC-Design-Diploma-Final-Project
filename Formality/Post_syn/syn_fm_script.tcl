b2e204a639687e85d6f66f684a52a5a5042aeb64
Ahmed Bakeer <ahmedhassan152004@gmail.com>
2025-10-18 19:52:15 +0300

    Add files via upload

---
+++ Formality/Post_syn/syn_fm_script.tcl
@@ -0,0 +1,91 @@
+
+########################### Define Top Module ############################
+                                                   
+set top_module SYS_TOP
+
+######################### Formality Setup File ###########################
+
+set synopsys_auto_setup true
+
+set_svf "../../Synthesis/$top_module.svf"
+
+
+set SSLIB "/home/IC/tsmc_fb_cl013g_sc/aci/sc-m/synopsys/scmetro_tsmc_cl013g_rvt_ss_1p08v_125c.db"
+set TTLIB "/home/IC/tsmc_fb_cl013g_sc/aci/sc-m/synopsys/scmetro_tsmc_cl013g_rvt_tt_1p2v_25c.db"
+set FFLIB "/home/IC/tsmc_fb_cl013g_sc/aci/sc-m/synopsys/scmetro_tsmc_cl013g_rvt_ff_1p32v_m40c.db"
+
+######################### Reference Container ############################
+
+## Read Reference technology libraries
+
+read_db -container Ref [list $SSLIB $TTLIB $FFLIB]
+
+## Read Reference Design Files
+
+read_verilog -container Ref "/home/IC/Projects/System/RTL/SYS_TOP.v"
+read_verilog -container Ref "/home/IC/Projects/System/RTL/ALU.v"
+read_verilog -container Ref "/home/IC/Projects/System/RTL/ASYNC_FIFO.v"
+read_verilog -container Ref "/home/IC/Projects/System/RTL/CLK_GATE.v"
+read_verilog -container Ref "/home/IC/Projects/System/RTL/CLKDIV_MUX.v"
+read_verilog -container Ref "/home/IC/Projects/System/RTL/Clock_Divider.v"
+read_verilog -container Ref "/home/IC/Projects/System/RTL/data_sampling.v"
+read_verilog -container Ref "/home/IC/Projects/System/RTL/DATA_SYNC.v"
+read_verilog -container Ref "/home/IC/Projects/System/RTL/deserializer.v"
+read_verilog -container Ref "/home/IC/Projects/System/RTL/DF_SYNC.v"
+read_verilog -container Ref "/home/IC/Projects/System/RTL/edge_bit_counter.v"
+read_verilog -container Ref "/home/IC/Projects/System/RTL/FIFO_MEM_CNTRL.v"
+read_verilog -container Ref "/home/IC/Projects/System/RTL/FIFO_RD.v"
+read_verilog -container Ref "/home/IC/Projects/System/RTL/FIFO_WR.v"
+read_sverilog -container Ref "/home/IC/Projects/System/RTL/FSM.sv"
+read_verilog -container Ref "/home/IC/Projects/System/RTL/MUX.v"
+read_verilog -container Ref "/home/IC/Projects/System/RTL/parity_calc.v"
+read_verilog -container Ref "/home/IC/Projects/System/RTL/parity_check.v"
+read_verilog -container Ref "/home/IC/Projects/System/RTL/PULSE_GEN.v"
+read_verilog -container Ref "/home/IC/Projects/System/RTL/Register_File.v"
+read_verilog -container Ref "/home/IC/Projects/System/RTL/RST_SYNC.v"
+read_verilog -container Ref "/home/IC/Projects/System/RTL/serializer.v"
+read_verilog -container Ref "/home/IC/Projects/System/RTL/start_check.v"
+read_verilog -container Ref "/home/IC/Projects/System/RTL/stop_check.v"
+read_sverilog -container Ref "/home/IC/Projects/System/RTL/SYS_CTRL.sv"
+read_verilog -container Ref "/home/IC/Projects/System/RTL/UART.v"
+read_verilog -container Ref "/home/IC/Projects/System/RTL/UART_RX.v"
+read_sverilog -container Ref "/home/IC/Projects/System/RTL/UART_RX_FSM.sv"
+read_verilog -container Ref "/home/IC/Projects/System/RTL/UART_TX.v"
+
+## set the top Reference Design 
+
+set_reference_design SYS_TOP
+set_top SYS_TOP
+
+######################## Implementation Container #########################
+
+## Read Implementation technology libraries
+
+read_db -container Imp [list $SSLIB $TTLIB $FFLIB]
+
+## Read Implementation Design Files
+ 
+read_verilog -container Imp -netlist "/home/IC/Projects/System/Synthesis/netlists/SYS_TOP.v"
+ 
+## set the top Implementation Design
+
+set_implementation_design SYS_TOP
+set_top SYS_TOP
+
+## matching Compare points
+match
+
+## verify
+set successful [verify]
+if {!$successful} {
+diagnose
+analyze_points -failing
+}
+
+report_passing_points > "reports/passing_points.rpt"
+report_failing_points > "reports/failing_points.rpt"
+report_aborted_points > "reports/aborted_points.rpt"
+report_unverified_points > "reports/unverified_points.rpt"
+
+
+start_gui
